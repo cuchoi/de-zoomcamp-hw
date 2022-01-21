cc3fdd400698c72a07837d9ab43e4ed84337b2b2
Fernando Irarrázaval <cuchoi@users.noreply.github.com>
2022-01-21 10:04:12 -0300

    Update q5.sql

--- hw1/q5.sql
+++ hw1/q5.sql
@@ -1,4 +1,5 @@
-SELECT count(*), pu_zone."Zone" as pu_zone, do_zone."Zone" as do_zone FROM yellow_taxi_data
+SELECT count(*), pu_zone."Zone" as pu_zone, do_zone."Zone" as do_zone
+FROM yellow_taxi_data
 LEFT JOIN zones pu_zone ON pu_zone."LocationID" = yellow_taxi_data."PULocationID"
 LEFT JOIN zones do_zone ON do_zone."LocationID" = yellow_taxi_data."DOLocationID"
 WHERE date(yellow_taxi_data.tpep_pickup_datetime) = '2021-01-14' AND yellow_taxi_data."PULocationID" = 43
