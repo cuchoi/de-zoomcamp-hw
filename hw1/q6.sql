1c00789d91f039fae8a9f5875a62bceda7b02487
Fernando Irarrázaval <cuchoi@users.noreply.github.com>
2022-01-21 10:22:22 -0300

    Update q6.sql

--- hw1/q6.sql
+++ hw1/q6.sql
@@ -1,4 +1,4 @@
-SELECT avg(yellow_taxi_data.total_amount), pu_zones."Zone", do_zones."Zone", count(*) FROM yellow_taxi_data
+SELECT AVG(yellow_taxi_data.total_amount), pu_zones."Zone", do_zones."Zone", COUNT(*) FROM yellow_taxi_data
 LEFT JOIN zones pu_zones ON pu_zones."LocationID" = yellow_taxi_data."PULocationID"
 LEFT JOIN zones do_zones ON do_zones."LocationID" = yellow_taxi_data."DOLocationID"
 GROUP BY 2, 3
