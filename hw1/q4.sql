ebbd2d9f1ac40fc9c67cd23908901dc696439f39
Fernando Irarrázaval <cuchoi@users.noreply.github.com>
2022-01-21 10:21:44 -0300

    Update q4.sql

--- hw1/q4.sql
+++ hw1/q4.sql
@@ -1,4 +1,4 @@
-SELECT max(tip_amount), date(tpep_pickup_datetime) FROM yellow_taxi_data 
-GROUP BY date(tpep_pickup_datetime)
+SELECT MAX(tip_amount), DATE(tpep_pickup_datetime) FROM yellow_taxi_data 
+GROUP BY DATE(tpep_pickup_datetime)
 ORDER BY 1 DESC 
 LIMIT 1;
